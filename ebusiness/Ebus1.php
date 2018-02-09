c1a3d5f099ab20bb6a3ef257ebd38361dbb97bbc
Gintaras <117337973@umail.ucc.ie>
2018-02-09 00:19:08 +0000

    finished nav bar and started design of homepage-subject to changes

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -4,7 +4,7 @@
         <title>Select Product</title>
         
         <!--jQuert-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery/.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="cost_calc.js"></script>
         
         
